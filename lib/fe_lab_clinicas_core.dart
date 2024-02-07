d403c37aef5a6aa3c31567f8c11eac0e93682f75
alinebrugnolli <aline.brugnolli@gmail.com>
2024-02-07 09:48:04 -0300

    Atualização dos export's

--- lib/fe_lab_clinicas_core.dart
+++ lib/fe_lab_clinicas_core.dart
@@ -4,7 +4,6 @@ export 'src/fp/either.dart';
 export 'src/fp/nil.dart';
 export 'src/fp/unit.dart';
 export 'src/helpers/messages.dart';
-
 export 'src/constants/local_storage_constants.dart';
 export 'src/lab_clinicas_core_config.dart';
 export 'src/exceptions/auth_exception.dart';
